e578f50d504a61c9a5192a0d598591347928ec79
Ilya Gruzhevski <ilya.gruzhevski@gmail.com>
2018-11-23 21:57:55 +0200

    Initial Commit

---
+++ AUTOmation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AUTOmation
+//
+//  Created by Ilya Gruzhevski on 23/11/2018.
+//  Copyright © 2018 Ilya Gruzhevski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
